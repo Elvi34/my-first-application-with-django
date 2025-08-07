acad452f222f8ed540a9d77525545ee512d31e0f
Elvi34 <elvirafozing34@gmail.com>
2025-08-07 12:59:27 +0100

    Ajoute mon projet

--- vente_fournitures_scolaires/vente_fournitures_scolaires/settings.py
+++ vente_fournitures_scolaires/vente_fournitures_scolaires/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.utils.translation import gettext_lazy as _
-
+from  decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yz%+m@)qv-*tpy66fdz)9$wcuyh#g!(o*x8sj_6wtrmt1vth(@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -102,16 +103,25 @@ USE_I18N = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nom_bdd',
-        'USER': 'postgres',
-        'PASSWORD': 'root',  
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nom_bdd',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',  
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
+DATABASES ={
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+
+
 }
+
+    
 
 
 # Password validation
