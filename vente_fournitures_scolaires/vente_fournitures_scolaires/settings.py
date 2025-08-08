c155ad68c2c4213d354f4eeffc597182dcd608fa
Elvi34 <elvirafozing34@gmail.com>
2025-08-08 16:05:20 +0100

    add

--- vente_fournitures_scolaires/vente_fournitures_scolaires/settings.py
+++ vente_fournitures_scolaires/vente_fournitures_scolaires/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = ['elviedushop.onrender.com']
+
 
 
 # Application definition
